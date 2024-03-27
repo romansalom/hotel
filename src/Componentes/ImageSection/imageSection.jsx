60516d914c96baed5389481607a331ee98eb00a8
rolo salom <roman.salom12@gmail.com>
2024-03-27 14:02:49 -0300

    modificando imagen

--- src/Componentes/ImageSection/imageSection.jsx
+++ src/Componentes/ImageSection/imageSection.jsx
@@ -1,138 +1,38 @@
 import React from 'react';
-import { Card, CardHeader, CardFooter, Image, Button } from '@nextui-org/react';
+import { Image } from '@nextui-org/react';
 
 export default function ImageSection() {
   return (
-    <div className="flex justify-center bg-cb bg-black">
-      <div className="max-w-[5000px] gap-5 grid grid-cols-12 grid-rows-2 px-8 mt-20 mb-20">
-        <Card className="col-span-12 sm:col-span-3 h-[300px] ">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              What to watch
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Stream the Acme event
-            </h4>
-          </CardHeader>
+    <div className="flex justify-center bg-black">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 mt-5">
+        <div className="relative">
           <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="foto4.jpg"
-          />
-        </Card>
-        <Card className="col-span-12 sm:col-span-3 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              What to watch
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Stream the Acme event
-            </h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="foto4.jpg"
-          />
-        </Card>
-        <Card className="col-span-12 sm:col-span-3 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              What to watch
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Stream the Acme event
-            </h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="foto4.jpg"
-          />
-        </Card>
-        <Card className="col-span-12 sm:col-span-3 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Plant a tree
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Contribute to the planet
-            </h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
+            className="h-auto max-w-full rounded-lg hover:filter hover:brightness-75 transition duration-300"
             src="foto6.jpg"
+            alt="Imagen 1"
           />
-        </Card>
-        <Card className="col-span-12 sm:col-span-3 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Plant a tree
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Contribute to the planet
-            </h4>
-          </CardHeader>
+        </div>
+        <div className="relative">
           <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
+            className="h-auto max-w-full rounded-lg hover:filter hover:brightness-75 transition duration-300"
             src="foto6.jpg"
+            alt="Imagen 2"
           />
-        </Card>
-        <Card className="col-span-12 sm:col-span-3 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Plant a tree
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Contribute to the planet
-            </h4>
-          </CardHeader>
+        </div>
+        <div className="relative">
           <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="foto6.jpg"
+            className="h-auto max-w-full rounded-lg hover:filter hover:brightness-75 transition duration-300"
+            src="header.jpg"
+            alt="Imagen 3"
           />
-        </Card>
-        <Card className="col-span-12 sm:col-span-3 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              What to watch
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Stream the Acme event
-            </h4>
-          </CardHeader>
+        </div>
+        <div className="relative">
           <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="foto4.jpg"
+            className="h-auto max-w-full rounded-lg hover:filter hover:brightness-75 transition duration-300"
+            src="foto5.jpeg"
+            alt="Imagen 4"
           />
-        </Card>
-        <Card className="col-span-12 sm:col-span-3 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              What to watch
-            </p>
-            <h4 className="text-white font-medium text-large">
-              Stream the Acme event
-            </h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="foto4.jpg"
-          />
-        </Card>
+        </div>
       </div>
     </div>
   );
