4ac43ca8684f633436db60e6e1b0632e12e57800
rolo salom <roman.salom12@gmail.com>
2024-04-04 15:55:29 -0300

    linea blanca

--- src/Componentes/Inversion/inversion.jsx
+++ src/Componentes/Inversion/inversion.jsx
@@ -6,14 +6,14 @@ function Inversion() {
   return (
     <div
       id="inversion"
-      className="mt-5 flex flex-wrap justify-center items-stretch  bg-dark-gray"
+      className="mt-5 flex flex-wrap justify-center items-stretch   bg-dark-gray"
     >
       {/* Primer componente */}
-      <div className="w-full sm:w-1/2 relative flex flex-col justify-center items-center  bg-dark-grays p-4">
+      <div className="w-full sm:w-1/2 relative flex flex-col justify-center items-center  border border-white bg-dark-grays p-4">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62093.38941661309!2d-58.27790821623154!3d-35.122144981444315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2c90e5d5eea8b%3A0x7224a0457f7205f7!2sVaca%20muerta%20PX!5e0!3m2!1ses-419!2sar!4v1712255687784!5m2!1ses-419!2sar"
           style={{ width: '100%', height: '100%', border: '0' }}
-          allowFullScreen=""
+          allowFullScreen="yes"
           title="iframwe"
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
@@ -21,7 +21,7 @@ function Inversion() {
       </div>
 
       {/* Segundo componente */}
-      <div className="w-full sm:w-1/2 flex flex-col justify-center items-center  bg-dark-gray p-4">
+      <div className="w-full sm:w-1/2 flex flex-col justify-center items-center   bg-dark-gray p-4">
         <h2 className="text-3xl lg:text-4xl text-white font-bold mt-10">
           ¿Querés saber más?
         </h2>
