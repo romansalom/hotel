3f2f1352845b52b2c054961d396b92feed3dc555
rolo salom <roman.salom12@gmail.com>
2024-06-12 14:36:46 -0300

    modificnaod

--- src/Componentes/BannerSuscribe/bannerSuscribe.jsx
+++ src/Componentes/BannerSuscribe/bannerSuscribe.jsx
@@ -3,15 +3,10 @@ import './banner.css';
 function BannnerSuscribe({ onClose }) {
   return (
     <div className="banner flex items-center justify-center fixed inset-0 bg-gray-800 bg-opacity-50 z-50">
-      <div className="relative flex flex-col items-center gap-y-6 p-8 bg-gradient-to-br from-[#404040] to-[#f2d6a2] text-white rounded-xl max-w-[408px] max-h-[374.4px] shadow-lg transform hover:scale-105 transition-transform duration-300">
+      <div className="relative flex flex-col items-center gap-y-3 p-5 bg-gradient-to-br from-[#404040] to-[#f2d6a2] text-white rounded-xl max-w-[408px] max-h-[374.4px] shadow-lg transform hover:scale-105 transition-transform duration-300">
         <div className="self-end">
           <button onClick={onClose} className="text-white">
-            <img
-              width="25.6"
-              height="25.6"
-              src="https://img.icons8.com/ios/50/ffffff/delete-sign--v1.png"
-              alt="Cerrar"
-            />
+            x
           </button>
         </div>
         <div className="text-center">
