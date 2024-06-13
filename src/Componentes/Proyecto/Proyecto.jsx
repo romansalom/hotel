cac251b372dd4c3241d7b6054db5aed1b83b3d85
rolo salom <roman.salom12@gmail.com>
2024-06-13 12:24:40 -0300

    agregando efectos

--- src/Componentes/Proyecto/Proyecto.jsx
+++ src/Componentes/Proyecto/Proyecto.jsx
@@ -1,17 +1,92 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Importa el archivo de estilos de AOS
-import { Image } from '@nextui-org/react';
 import './proyecto.css';
 
 const ResponsiveImageTextComponent = () => {
+  const [images] = useState(['header.jpg', 'foto5.jpeg', 'foto6.jpg']);
+  const [images2] = useState(['header.jpg', 'foto5.jpeg', 'foto6.jpg']);
+  const [images3] = useState(['header.jpg', 'foto5.jpeg', 'foto6.jpg']);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(0);
+  const [currentIndex3, setCurrentIndex3] = useState(0);
+
+  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible2, setIsVisible2] = useState(true);
+  const [isVisible3, setIsVisible3] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     AOS.init({
-      duration: 1000, // Duración de la animación en milisegundos
-      once: true, // La animación se ejecutará solo una vez
-      easing: 'ease-in-out', // Efecto de easing
+      duration: 1000,
+      once: false,
+      easing: 'ease-in-out',
     });
-  }, []); // El array vacío asegura que el efecto se ejecute solo una vez cuando el componente se monta
+
+    // Preload all images
+    const preloadImages = () => {
+      images.forEach((src) => {
+        const img = new window.Image();
+        img.src = src;
+      });
+    };
+    const preloadImages2 = () => {
+      images2.forEach((src) => {
+        const img2 = new window.Image();
+        img2.src = src;
+      });
+    };
+    const preloadImages3 = () => {
+      images3.forEach((src) => {
+        const img3 = new window.Image();
+        img3.src = src;
+      });
+    };
+    preloadImages();
+    preloadImages2();
+    preloadImages3();
+  }, [images, images2, images3]);
+
+  // Handle image load to update state
+  const handleImageLoad = () => {
+    setIsLoading(false);
+    setIsVisible(true); // Make the new image visible after load
+  };
+  const handleImageLoad2 = () => {
+    setIsLoading(false);
+    setIsVisible2(true); // Make the new image visible after load
+  };
+  const handleImageLoad3 = () => {
+    setIsLoading(false);
+    setIsVisible3(true); // Make the new image visible after load
+  };
+
+  // Handle image click to change to the next image
+  const handleImageClick = (e) => {
+    e.preventDefault();
+    setIsVisible(false); // Start the transition
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setIsLoading(true); // Prepare to load the new image
+    }, 500); // Wait for the fade-out transition to complete
+  };
+  const handleImageClick2 = (e) => {
+    e.preventDefault();
+    setIsVisible2(false); // Start the transition
+    setTimeout(() => {
+      setCurrentIndex2((prevIndex) => (prevIndex + 1) % images2.length);
+      setIsLoading(true); // Prepare to load the new image
+    }, 500); // Wait for the fade-out transition to complete
+  };
+  const handleImageClick3 = (e) => {
+    e.preventDefault(); // Prevent the default behavior
+    setIsVisible3(false); // Start the transition
+    setTimeout(() => {
+      setCurrentIndex3((prevIndex) => (prevIndex + 1) % images3.length);
+      setIsLoading(true); // Prepare to load the new image
+    }, 500); // Wait for the fade-out transition to complete
+  };
 
   return (
     <div className="contenedores">
@@ -22,19 +97,27 @@ const ResponsiveImageTextComponent = () => {
         <br />
         <br />
         <br />
+        {/* Primer bloque de contenido */}
         <div
           className="flex flex-col md:flex-row items-center justify-center"
-          data-aos="fade-up" // Aplicamos el efecto de aparición
+          data-aos="fade-up"
         >
-          <div className="md:ml-2.5 lg:ml-2.5 xl:ml-2.5 2xl:ml-2.5 relative mb-4 md:mb-0 hover:scale-10 hover:-translate-y-2 hover:shadow-2xl transition duration-300 ease-in-out">
-            <Image
-              src="header.jpg"
-              className="w-full h-full md:w-full lg:w-full xl:w-full 2xl:w-full"
-              layout="fill"
-              objectFit="cover"
-            />
+          <div className="md:ml-2.5 lg:ml-2.5 xl:ml-2.5 2xl:ml-2.5  relative mb-4 md:mb-0 hover:scale-105 hover:shadow-2xl transition duration-300 ease-in-out">
+            <div
+              className="image-container"
+              onClick={(e) => handleImageClick(e)}
+            >
+              <img
+                src={images[currentIndex]}
+                alt={`Imagen ${currentIndex + 1}`}
+                className={`image ${
+                  isVisible ? 'image-visible' : 'image-hidden'
+                }`}
+                onLoad={handleImageLoad}
+                style={{ display: isLoading ? 'none' : 'block' }}
+              />
+            </div>
           </div>
-
           <div className="md:ml-8 lg:ml-8 xl:ml-8 2xl:ml-8 font-[sans-serif]">
             <h2 className="text-2xl font-extrabold">Título del Texto</h2>
             <br />
@@ -55,19 +138,27 @@ const ResponsiveImageTextComponent = () => {
         <br />
         <br />
         <br />
+        {/* Segundo bloque de contenido */}
         <div
           className="flex flex-col md:flex-row items-center justify-center font-[sans-serif]"
           data-aos="fade-up"
         >
-          <div className="md:ml-2 lg:ml-2 xl:ml-2 2xl:ml-2 relative mb-4 md:mb-0 md:order-2 lg:order-2 xl:order-2 2xl:order-2 hover:scale-10 hover:-translate-y-2 hover:shadow-2xl transition duration-300 ease-in-out">
-            <Image
-              src="foto5.jpeg"
-              className="w-full h-full md:w-full lg:w-full xl:w-full 2xl:w-full"
-              layout="fill"
-              objectFit="cover"
-            />
+          <div className="md:ml-2 lg:ml-2 xl:ml-2 2xl:ml-2 relative mb-4 md:mb-0 md:order-2 lg:order-2 xl:order-2 2xl:order-2 hover:scale-105 hover:shadow-2xl transition duration-300 ease-in-out">
+            <div
+              className="image-container"
+              onClick={(e) => handleImageClick2(e)}
+            >
+              <img
+                src={images2[(currentIndex2 + 1) % images2.length]}
+                alt={`Imagen ${currentIndex2 + 2}`}
+                className={`image ${
+                  isVisible2 ? 'image-visible' : 'image-hidden'
+                }`}
+                onLoad={handleImageLoad2}
+                style={{ display: isLoading ? 'none' : 'block' }}
+              />
+            </div>
           </div>
-
           <div className="md:ml-8 lg:ml-8 xl:ml-8 2xl:ml-8 font-[sans-serif]">
             <h2 className="text-2xl font-extrabold">Título del Texto</h2>
             <br />
@@ -88,20 +179,28 @@ const ResponsiveImageTextComponent = () => {
         <br />
         <br />
         <br />
+        {/* Tercer bloque de contenido */}
         <div
-          className="flex flex-col md:flex-row items-center justify-center font-[sans-serif]"
+          className="flex flex-col md:flex-row items-center justify-center"
           data-aos="fade-up"
         >
-          <div className="md:ml-2.5 lg:ml-2.5 xl:ml-2.5 2xl:ml-2.5 relative mb-4 md:mb-0 hover:scale-10 hover:-translate-y-2 hover:shadow-2xl transition duration-300 ease-in-out">
-            <Image
-              src="foto6.jpg"
-              className="w-full h-full md:w-full lg:w-full xl:w-full 2xl:w-full"
-              layout="fill"
-              objectFit="cover"
-            />
+          <div className="md:ml-2.5 lg:ml-2.5 xl:ml-2.5 2xl:ml-2.5  relative mb-4 md:mb-0 hover:scale-105 hover:shadow-2xl transition duration-300 ease-in-out">
+            <div
+              className="image-container"
+              onClick={(e) => handleImageClick3(e)}
+            >
+              <img
+                src={images3[currentIndex3]}
+                alt={`Imagen ${currentIndex3 + 1}`}
+                className={`image ${
+                  isVisible3 ? 'image-visible' : 'image-hidden'
+                }`}
+                onLoad={handleImageLoad3}
+                style={{ display: isLoading ? 'none' : 'block' }}
+              />
+            </div>
           </div>
-
-          <div className="md:ml-8 lg:ml-8 xl:ml-8 2xl:ml-8">
+          <div className="md:ml-8 lg:ml-8 xl:ml-8 2xl:ml-8 font-[sans-serif]">
             <h2 className="text-2xl font-extrabold">Título del Texto</h2>
             <br />
             <p className="text-lg">
