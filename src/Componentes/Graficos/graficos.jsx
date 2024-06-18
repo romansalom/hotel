70e306ae38d890e56207099449900a9a1a01fcb7
rolo salom <roman.salom12@gmail.com>
2024-06-18 15:14:11 -0300

    modificnado

--- src/Componentes/Graficos/graficos.jsx
+++ src/Componentes/Graficos/graficos.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Line } from 'react-chartjs-2';
 import 'intersection-observer';
 import './investmentComparisonChart.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -28,6 +26,8 @@ ChartJS.register(
 const InvestmentComparisonChart = () => {
   const [chartData, setChartData] = useState(null);
   const [animationKey, setAnimationKey] = useState(0);
+  const chartRef = useRef(null); // Referencia para el componente gráfico
+  const [isIntersecting, setIsIntersecting] = useState(false); // Estado para saber si el componente está en vista
 
   const data = {
     labels: Array.from({ length: 50 }, (_, i) => i + 1),
@@ -35,36 +35,36 @@ const InvestmentComparisonChart = () => {
       {
         label: 'Inversión A',
         data: [
-          1500, 6700, 1900, 2100, 2300, 2500, 2700, 2900, 5100, 3300, 3500,
-          3700, 3900, 4100, 4300, 4500, 2300, 4900, 5100, 5300, 5500, 5700,
-          5900, 6100, 1900, 6500, 6700, 6900, 4100, 7300, 2000, 5400, 2400,
-          2600, 2800, 3000, 3200, 3400, 3600, 3800, 4000, 4200, 7200, 4600,
-          4800, 2400, 5200, 5400, 7400, 5800, 6000, 6200, 6400, 6600, 6800,
-          4800, 7200, 7400, 7600, 7800,
+          0, 6700, 1900, 2100, 2300, 2500, 2700, 2900, 5100, 3300, 3500, 3700,
+          3900, 4100, 4300, 4500, 2300, 4900, 5100, 5300, 5500, 5700, 5900,
+          6100, 1900, 6500, 6700, 6900, 4100, 7300, 2000, 5400, 2400, 2600,
+          2800, 3000, 3200, 3400, 3600, 3800, 4000, 4200, 7200, 4600, 4800,
+          2400, 5200, 5400, 7400, 5800, 6000, 6200, 6400, 6600, 6800, 4800,
+          7200, 7400, 7600, 7800,
         ],
         fill: false,
         backgroundColor: 'rgba(0, 0, 0, 0.2)', // Fondo negro con transparencia
         borderColor: 'rgb(0, 0, 0)',
-        borderWidth: 5, // Aumentar el grosor de la línea
-        tension: 0.3,
+        borderWidth: 7, // Aumentar el grosor de la línea
+        tension: 0.6,
         pointRadius: 0, // Tamaño de los puntos (0 para eliminarlos)
         pointHoverRadius: 0, // Curvar la línea para un efecto parabólico
       },
       {
         label: 'Inversión B',
         data: [
-          2000, 5400, 2400, 2600, 2800, 3000, 3200, 3400, 3600, 3800, 4000,
-          4200, 7200, 4600, 4800, 2400, 5200, 5400, 7400, 5800, 6000, 6200,
-          6400, 6600, 6800, 4800, 7200, 7400, 7600, 7800, 1500, 6700, 1900,
-          2100, 2300, 2500, 2700, 2900, 5100, 3300, 3500, 3700, 3900, 4100,
-          4300, 4500, 2300, 4900, 5100, 5300, 5500, 5700, 5900, 6100, 1900,
-          6500, 6700, 6900, 4100, 7300,
+          0, 5400, 2400, 2600, 2800, 3000, 3200, 3400, 3600, 3800, 4000, 4200,
+          7200, 4600, 4800, 2400, 5200, 5400, 7400, 5800, 6000, 6200, 6400,
+          6600, 6800, 4800, 7200, 7400, 7600, 7800, 1500, 6700, 1900, 2100,
+          2300, 2500, 2700, 2900, 5100, 3300, 3500, 3700, 3900, 4100, 4300,
+          4500, 2300, 4900, 5100, 5300, 5500, 5700, 5900, 6100, 1900, 6500,
+          6700, 6900, 4100, 7300,
         ],
         fill: false,
         backgroundColor: 'rgba(255, 215, 0, 0.2)', // Fondo dorado con transparencia
         borderColor: 'rgb(255, 215, 0)', // Borde dorado
-        borderWidth: 6, // Aumentar el grosor de la línea
-        tension: 0.4,
+        borderWidth: 7, // Aumentar el grosor de la línea
+        tension: 0.6,
         pointRadius: 0, // Tamaño de los puntos (0 para eliminarlos)
         pointHoverRadius: 0, // Curvar la línea para un efecto parabólico
       },
@@ -72,15 +72,38 @@ const InvestmentComparisonChart = () => {
   };
 
   useEffect(() => {
-    // Cargar los datos del gráfico al iniciar
-    setChartData(data);
+    setChartData(data); // Cargar los datos del gráfico al iniciar
+
+    // Configurar el Intersection Observer
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsIntersecting(true);
+          }
+        });
+      },
+      {
+        threshold: 1.0, // El componente debe estar completamente visible
+      }
+    );
+
+    if (chartRef.current) {
+      observer.observe(chartRef.current); // Observar el componente de gráfico
+    }
+
+    return () => {
+      if (chartRef.current) {
+        observer.unobserve(chartRef.current); // Dejar de observar el componente al desmontar
+      }
+    };
   }, []);
 
   const handleRepeatAnimation = () => {
     setAnimationKey((prevKey) => prevKey + 1); // Incrementa la clave para reiniciar la animación
   };
 
-  const totalDuration = 3000; // Duración más rápida para la animación
+  const totalDuration = 6000; // Duración más lenta para la animación
   const delayBetweenPoints = totalDuration / data.labels.length;
   const previousY = (ctx) =>
     ctx.index === 0
@@ -139,54 +162,31 @@ const InvestmentComparisonChart = () => {
     },
   };
 
-  useEffect(() => {
-    AOS.init({
-      once: true,
-    }); // Inicializa AOS con animaciones una sola vez
-  }, []);
-
   return (
-    <div className="componentesa" data-aos="fade-up">
-      <div className="chart-container" data-aos="fade-up">
-        <div class="bg-white-100 py-12 px-4 font-[sans-serif] text-[#333]">
-          <div class="max-w-4xl mx-auto">
-            <h2 class="text-4xl font-extrabold mb-5 text-center">
+    <div className="componentesa" ref={chartRef}>
+      <div className="chart-container">
+        <div className="bg-white-100 py-12 px-4 font-[sans-serif] text-[#333]">
+          <div className="max-w-4xl mx-auto">
+            <h2 className="text-4xl font-extrabold mb-5 text-center">
               Inverti En Vacamuerta
             </h2>
-            <h4 class="text-2xl font-bold mb-12 text-center">
-              Comparando inversion con Capital al pasar del tiempo
+            <h4 className="text-2xl font-bold mb-12 text-center">
+              Comparando inversión con Capital al pasar del tiempo
             </h4>
-            <button onClick={handleRepeatAnimation} className="repeat-button">
-              <svg
-                class="w-6 h-6 text-gray-800 dark:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8 18V6l8 6-8 6Z"
-                />
-              </svg>
-            </button>
           </div>
         </div>
 
-        {chartData && (
-          <Line
-            key={animationKey}
-            data={chartData}
-            options={chartOptions}
-            width={400} // Ancho fijo para el gráfico
-            height={300} // Alto fijo para el gráfico
-          />
-        )}
+        {chartData &&
+          isIntersecting && ( // Solo renderizar el gráfico cuando esté en la vista
+            <Line
+              onClick={handleRepeatAnimation}
+              key={animationKey}
+              data={chartData}
+              options={chartOptions}
+              width={400} // Ancho fijo para el gráfico
+              height={300} // Alto fijo para el gráfico
+            />
+          )}
       </div>
     </div>
   );
