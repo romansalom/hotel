3af9bead8805ccbae21b5f8cf108c679a885fa38
rolo salom <roman.salom12@gmail.com>
2024-06-23 18:31:28 -0300

    modificando todo

--- src/Componentes/Inversion/inversion.jsx
+++ src/Componentes/Inversion/inversion.jsx
@@ -6,8 +6,6 @@ import VideoBackground from '../BannerVideo/bannerVideo';
 function Contacto() {
   return (
     <div id="contacto" className="contenedores1-zoom">
-      <br></br>
-      <br></br>
       <div class="grid md:grid-cols-2 gap-16 items-center relative overflow-hidden p-10 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-3xl max-w-6xl mx-auto bg-white text-[#333] my-6 font-[sans-serif] before:absolute before:right-0 before:w-[300px] before:bg-orange-600 before:h-full max-md:before:hidden">
         <div>
           <VideoBackground></VideoBackground>
