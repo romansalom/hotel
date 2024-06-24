83d05b781eeaa8ee128c2b0a644239803143c8ba
rolo salom <roman.salom12@gmail.com>
2024-06-24 13:14:40 -0300

    agregando colores

--- src/Componentes/faq/faq.jsx
+++ src/Componentes/faq/faq.jsx
@@ -4,17 +4,7 @@ const Faqs = () => {
   const [search, setSearch] = useState('');
   const [openIndex, setOpenIndex] = useState(-1); // Track which FAQ item is open
   const [filteredFaqItems, setFilteredFaqItems] = useState([]); // State for filtered items
-
   const faqItems = [
-    {
-      question: '¿Qué es Lorem Ipsum?',
-      answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
-    },
-    {
-      question: '¿Por qué lo usamos?',
-      answer:
-        'Es un hecho establecido hace mucho tiempo que un lector será distraído por...',
-    },
     {
       question: '¿Cuáles son los horarios de check-in y check-out?',
       answer:
@@ -84,48 +74,49 @@ const Faqs = () => {
 
   return (
     <div className="mx-auto max-w-6xl p-2 rounded">
-      <div className="text-center text-3xl text-green-400 mt-10 mb-10">
-        <h1 className="text-4xl font-bold text-black">Preguntas Frecuentes</h1>
+      <div className="flex justify-center items-center flex-col mt-10">
+        <h1 className="text-4xl font-bold text-black mb-6">
+          Preguntas Frecuentes
+        </h1>
+
+        <div className="w-full max-w-xs relative text-gray-600">
+          <input
+            value={search}
+            onChange={handleSearch}
+            type="search"
+            name="search"
+            placeholder="Buscar"
+            className="bg-white w-full h-10 px-5 rounded-full text-sm focus:outline-none"
+          />
+          <button
+            type="submit"
+            className="focus:outline-none absolute right-0 top-0 mt-3 mr-4"
+          >
+            <svg
+              className="h-4 w-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              id="Capa_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 56.966 56.966"
+              style={{ enableBackground: 'new 0 0 56.966 56.966' }}
+              xmlSpace="preserve"
+              width="512px"
+              height="512px"
+            >
+              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23 s-23,10.318-23,23s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92c0.779,0,1.518-0.297,2.079-0.837 C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+          </button>
+        </div>
       </div>
 
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/3 md:p-4 text-sm sticky inset-x-0 top-0 left-0 py-12">
-          <div className="relative text-gray-600 mt-8 lg:mr-16">
-            <input
-              value={search}
-              onChange={handleSearch}
-              type="search"
-              name="search"
-              placeholder="Buscar"
-              className="bg-white w-full h-10 px-5 rounded-full text-sm focus:outline-none"
-            />
-            <button
-              type="submit"
-              className="focus:outline-none absolute right-0 top-0 mt-3 mr-4"
-            >
-              <svg
-                className="h-4 w-4 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                version="1.1"
-                id="Capa_1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 56.966 56.966"
-                style={{ enableBackground: 'new 0 0 56.966 56.966' }}
-                xmlSpace="preserve"
-                width="512px"
-                height="512px"
-              >
-                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23 s-23,10.318-23,23s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92c0.779,0,1.518-0.297,2.079-0.837 C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17s-17-7.626-17-17S14.61,6,23.984,6z" />
-              </svg>
-            </button>
-          </div>
-        </div>
-        <div className="md:w-2/3">
+      <div className="mt-6">
+        <div className="flex flex-col">
           {filteredFaqItems.map((item, index) => (
             <div key={index} className="p-4">
-              <div className="item px-6 py-2">
+              <div className="item">
                 <a
                   href="#"
                   className="flex items-center justify-between"
@@ -136,7 +127,9 @@ const Faqs = () => {
                 >
                   <h4
                     className={
-                      openIndex === index ? 'text-[#F2D6A2] font-medium' : ''
+                      openIndex === index
+                        ? 'text-[#F2D6A2] font-medium'
+                        : 'font-medium'
                     }
                   >
                     {item.question}
