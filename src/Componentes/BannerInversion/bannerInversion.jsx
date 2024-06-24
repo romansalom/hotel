6822def3e6280f3249443c81e81fbc5519e2ab34
rolo salom <roman.salom12@gmail.com>
2024-06-24 11:14:44 -0300

    agregando colores

--- src/Componentes/BannerInversion/bannerInversion.jsx
+++ src/Componentes/BannerInversion/bannerInversion.jsx
@@ -2,12 +2,10 @@ import InvestmentComparisonChart from '../Graficos/graficos';
 
 const BannerINversion = () => {
   return (
-    <div class="bg-black px-8 py-14 font-[sans-serif] text-gray-300">
+    <div class="bg-[#404040] px-8 py-14 font-[sans-serif] text-gray-300">
       <div class="grid md:grid-cols-2 items-center gap-12 max-w-6xl mx-auto">
         <div>
-          <h1 class="text-4xl font-bold text-emerald-500">
-            Startup Website Template
-          </h1>
+          <h1 class="text-4xl font-bold text-[#f2d6a2]">Startup Website</h1>
           <p class="mt-6 text-ml">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
             accumsan, nunc et tempus blandit, metus mi consectetur nibh, a
@@ -16,36 +14,36 @@ const BannerINversion = () => {
           </p>
         </div>
         <div class="grid sm:grid-cols-2 gap-6">
-          <div class="bg-[#3e3e3e] flex flex-col items-center text-center rounded md:p-8 p-6">
-            <h3 class="lg:text-5xl text-3xl font-extrabold text-emerald-500">
+          <div class="bg-black flex flex-col items-center text-center rounded md:p-8 p-6">
+            <h3 class="lg:text-5xl text-3xl font-extrabold text-[#f2d6a2]">
               5.4
             </h3>
             <div class="mt-4">
-              <p class="text-sm">Total Users</p>
+              <p class="text-sm text-[#f2d6a2]">Total Users</p>
             </div>
           </div>
-          <div class="bg-[#3e3e3e] flex flex-col items-center text-center rounded md:p-8 p-6">
-            <h3 class="lg:text-5xl text-3xl font-extrabold text-emerald-500">
+          <div class="bg-black flex flex-col items-center text-center rounded md:p-8 p-6">
+            <h3 class="lg:text-5xl text-3xl font-extrabold text-[#f2d6a2]">
               $80K
             </h3>
             <div class="mt-4">
-              <p class="text-sm">Revenue</p>
+              <p class="text-sm text-[#f2d6a2]">Revenue</p>
             </div>
           </div>
-          <div class="bg-[#3e3e3e] flex flex-col items-center text-center rounded md:p-8 p-6">
-            <h3 class="lg:text-5xl text-3xl font-extrabold text-emerald-500">
+          <div class="bg-black flex flex-col items-center text-center rounded md:p-8 p-6">
+            <h3 class="lg:text-5xl text-3xl font-extrabold  text-[#f2d6a2]">
               100K
             </h3>
             <div class="mt-4">
-              <p class="text-sm">Engagement</p>
+              <p class="text-sm text-[#f2d6a2]">Engagement</p>
             </div>
           </div>
-          <div class="bg-[#3e3e3e] flex flex-col items-center text-center rounded md:p-8 p-6">
-            <h3 class="lg:text-5xl text-3xl font-extrabold text-emerald-500">
+          <div class="bg-black flex flex-col items-center text-center rounded md:p-8 p-6">
+            <h3 class="lg:text-5xl text-3xl font-extrabold  text-[#f2d6a2]">
               99.9%
             </h3>
             <div class="mt-4">
-              <p class="text-sm">Server Uptime</p>
+              <p class="text-sm text-[#f2d6a2]">Server Uptime</p>
             </div>
           </div>
         </div>
--- src/Componentes/Inversion/inversion.jsx
+++ src/Componentes/Inversion/inversion.jsx
@@ -6,11 +6,11 @@ import VideoBackground from '../BannerVideo/bannerVideo';
 function Contacto() {
   return (
     <div id="contacto" className="contenedores1-zoom">
-      <div class="grid md:grid-cols-2 gap-16 items-center relative overflow-hidden p-10 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-3xl max-w-6xl mx-auto bg-white text-[#333] my-6 font-[sans-serif] before:absolute before:right-0 before:w-[300px] before:bg-orange-600 before:h-full max-md:before:hidden">
+      <div class="grid md:grid-cols-2 gap-16 items-center relative overflow-hidden p-10 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-3xl max-w-6xl mx-auto bg-[#F2F2F2] text-[#333] my-6 font-[sans-serif] before:absolute before:right-0 before:w-[300px] before:bg-[#f2d6a2] before:h-full max-md:before:hidden">
         <div>
           <VideoBackground></VideoBackground>
 
-          <p class="text-sm text-gray-400 mt-3">
+          <p class="text-sm text-black-400 mt-3">
             ¿Tienes una consulta específica o estás buscando explorar nuevas
             oportunidades? Nuestro equipo experimentado está listo para
             colaborar contigo.
