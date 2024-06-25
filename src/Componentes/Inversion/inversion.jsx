dcc85cae9a1320778c8c16b14660a83da41f939e
rolo salom <roman.salom12@gmail.com>
2024-06-25 15:13:34 -0300

    los contactos funcioann perfectamwnte

--- src/Componentes/Inversion/inversion.jsx
+++ src/Componentes/Inversion/inversion.jsx
@@ -1,80 +1,179 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
 import './inversion.css'; // Asegúrate de importar tu archivo CSS aquí
 import { Input, Textarea } from '@nextui-org/react';
 import VideoBackground from '../BannerVideo/bannerVideo';
 import VideoBackgroundVaca from '../BannerVideoVacamuerta/bannerVideo';
 
 function Contacto() {
+  const [isSending, setIsSending] = useState(false); // Estado para indicar si se está enviando el formulario
+  const [errorMessage, setErrorMessage] = useState(''); // Estado para mensaje de error
+  const [successMessage, setSuccessMessage] = useState(''); // Estado para mensaje de éxito
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    // Validar campos vacíos
+    const { from_name, from_mobile, from_email, mensaje } = e.target.elements;
+    if (
+      !from_name.value ||
+      !from_mobile.value ||
+      !from_email.value ||
+      !mensaje.value
+    ) {
+      setErrorMessage('Todos los campos son obligatorios');
+      return;
+    }
+
+    // Validar número de teléfono usando regex
+    const telefonoRegex =
+      /^\+?(\d{1,3})?[-. ]?\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+    if (!telefonoRegex.test(from_mobile.value)) {
+      setErrorMessage('Por favor, introduce un número de teléfono válido');
+      return;
+    }
+
+    setIsSending(true); // Establece el estado de carga a true
+    setErrorMessage('');
+    setSuccessMessage('');
+
+    emailjs
+      .sendForm(
+        'servicioContacto',
+        'template_f6650pa',
+        e.target,
+        'PxlqQfCYGvW0q_OMs'
+      )
+      .then((result) => {
+        console.log(result.text);
+        setSuccessMessage('Mensaje enviado correctamente');
+      })
+      .catch((error) => {
+        console.error('Error al enviar el mensaje:', error);
+        setErrorMessage('Error al enviar el mensaje');
+      })
+      .finally(() => {
+        setIsSending(false); // Establece el estado de carga de nuevo a false después del envío
+        e.target.reset(); // Reinicia el formulario
+      });
+  }
+
+  // Función para limpiar el mensaje de error cuando se edita un campo específico
+  function handleInputChange() {
+    if (errorMessage) {
+      setErrorMessage('');
+    }
+  }
+
   return (
     <div id="contacto" className="contenedores1-zoom">
-      <div class="grid md:grid-cols-2 gap-16 items-center relative overflow-hidden p-10 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-3xl max-w-6xl mx-auto bg-[#F2F2F2] text-[#333] my-6 font-[sans-serif] before:absolute before:right-0 before:w-[300px] before:bg-[#f2d6a2] before:h-full max-md:before:hidden">
+      <div className="grid md:grid-cols-2 gap-16 items-center relative overflow-hidden p-10 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-3xl max-w-6xl mx-auto bg-[#F2F2F2] text-[#333] my-6 font-[sans-serif] before:absolute before:right-0 before:w-[300px] before:bg-[#f2d6a2] before:h-full max-md:before:hidden">
         <div>
-          <VideoBackground></VideoBackground>
-
-          <p class="text-sm text-black-400 mt-3">
+          <VideoBackground />
+          <p className="text-sm text-black-400 mt-3">
             ¿Tienes una consulta específica o estás buscando explorar nuevas
             oportunidades? Nuestro equipo experimentado está listo para
             colaborar contigo.
           </p>
-          <form>
-            <div class="space-y-4 mt-8">
+          <form onSubmit={sendEmail}>
+            <div className="space-y-4 mt-8">
               <input
                 type="text"
-                placeholder="Full Name"
-                class="px-2 py-3 bg-white w-full text-sm border-b-2 focus:border-[#333] outline-none"
+                name="from_name"
+                placeholder="Nombre completo"
+                className="px-2 py-3 bg-white w-full text-sm border-b-2 focus:border-[#f2d6a2] outline-none"
+                onChange={handleInputChange}
               />
 
               <input
-                type="number"
-                placeholder="Phone No."
-                class="px-2 py-3 bg-white text-black w-full text-sm border-b-2 focus:border-[#333] outline-none"
+                type="tel"
+                name="from_mobile"
+                placeholder="Teléfono"
+                className="px-2 py-3 bg-white text-black w-full text-sm border-b-2 focus:border-[#f2d6a2] outline-none"
+                onChange={handleInputChange}
               />
 
               <input
                 type="email"
+                name="from_email"
                 placeholder="Email"
-                class="px-2 py-3 bg-white text-black w-full text-sm border-b-2 focus:border-[#333] outline-none"
+                className="px-2 py-3 bg-white text-black w-full text-sm border-b-2 focus:border-[#f2d6a2] outline-none"
+                onChange={handleInputChange}
               />
 
               <textarea
-                placeholder="Write Message"
-                class="px-2 pt-3 bg-white text-black w-full text-sm border-b-2 focus:border-[#333] outline-none"
+                name="mensaje"
+                placeholder="Escribe tu mensaje"
+                className="px-2 pt-3 bg-white text-black w-full text-sm border-b-2 focus:border-[#f2d6a2] outline-none"
+                onChange={handleInputChange}
               ></textarea>
             </div>
+            {errorMessage && (
+              <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+            )}
+            {successMessage && (
+              <p className="text-green-500 text-sm mt-2">{successMessage}</p>
+            )}
             <button
-              type="button"
-              class="mt-8 flex items-center justify-center text-sm w-full rounded px-4 py-2.5 font-semibold bg-[#333] text-white hover:bg-[#222]"
+              type="submit"
+              className={`mt-8 flex items-center justify-center text-sm w-full rounded px-4 py-2.5 font-semibold bg-[#333] text-white hover:bg-[#222] ${
+                isSending ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+              disabled={isSending}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16px"
-                height="16px"
-                fill="#fff"
-                class="mr-2"
-                viewBox="0 0 548.244 548.244"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M392.19 156.054 211.268 281.667 22.032 218.58C8.823 214.168-.076 201.775 0 187.852c.077-13.923 9.078-26.24 22.338-30.498L506.15 1.549c11.5-3.697 24.123-.663 32.666 7.88 8.542 8.543 11.577 21.165 7.879 32.666L390.89 525.906c-4.258 13.26-16.575 22.261-30.498 22.338-13.923.076-26.316-8.823-30.728-22.032l-63.393-190.153z"
-                  clip-rule="evenodd"
-                  data-original="#000000"
-                />
-              </svg>
-              Enviar mensaje
+              {isSending ? (
+                <svg
+                  className="animate-spin h-5 w-5 mr-3"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V2.5a.5.5 0 10-1 0V4a7 7 0 00-7 7h1.5a.5.5 0 000-1H2.5a.5.5 0 00-.5.5v1.5a.5.5 0 001 0V12zm10 0a8 8 0 01-8 8V21.5a.5.5 0 101 0V20a7 7 0 007-7h-1.5a.5.5 0 000 1h1.5a.5.5 0 00.5-.5v-1.5a.5.5 0 10-1 0V12z"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16px"
+                  height="16px"
+                  fill="#fff"
+                  className="mr-2"
+                  viewBox="0 0 548.244 548.244"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M392.19 156.054 211.268 281.667 22.032 218.58C8.823 214.168-.076 201.775 0 187.852c.077-13.923 9.078-26.24 22.338-30.498L506.15 1.549c11.5-3.697 24.123-.663 32.666 7.88 8.542 8.543 11.577 21.165 7.879 32.666L390.89 525.906c-4.258 13.26-16.575 22.261-30.498 22.338-13.923.076-26.316-8.823-30.728-22.032l-63.393-190.153z"
+                    clipRule="evenodd"
+                    data-original="#000000"
+                  />
+                </svg>
+              )}
+              {isSending ? 'Enviando...' : 'Enviar mensaje'}
             </button>
           </form>
         </div>
-        <div class="z-10 relative h-full max-md:min-h-[350px]">
+        <div className="z-10 relative h-full max-md:min-h-[350px]">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62093.38941661309!2d-58.27790821623154!3d-35.122144981444315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2c90e5d5eea8b%3A0x7224a0457f7205f7!2sVaca%20muerta%20PX!5e0!3m2!1ses-419!2sar!4v1712255687784!5m2!1ses-419!2sar"
-            class="left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg"
-            frameborder="0"
-            allowfullscreen
+            className="left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg"
+            frameBorder="0"
+            allowFullScreen
           ></iframe>
         </div>
       </div>
-      <br></br>
-      <br></br>
+      <br />
+      <br />
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import NavBar from '../Componentes/NavBar/seccionBlanca';
 import Headers from '../Componentes/Header/header';
 import ResponsiveImageTextComponent from '../Componentes/Proyecto/Proyecto';
 import Contacto from '../Componentes/Inversion/inversion';
-import ImageSection from '../Componentes/ImageSection/imageSection';
-import Características from '../Componentes/Características/características';
-import Banner from '../Componentes/Banner/banner';
-import Footer from '../Componentes/Footer/footer';
-import './home.css';
-import SeccionBlanca from '../Componentes/NavBar/seccionBlanca';
-import BannnerSuscribe from '../Componentes/BannerSuscribe/bannerSuscribe';
-import VideoBackground from '../Componentes/BannerVideo/bannerVideo';
-import InvestmentComparisonChart from '../Componentes/Graficos/graficos';
 import BannerINversion from '../Componentes/BannerInversion/bannerInversion';
-import Faq from '../Componentes/faq/faq';
 import Faqs from '../Componentes/faq/faq';
+import BannnerSuscribe from '../Componentes/BannerSuscribe/bannerSuscribe';
+import './home.css';
+import Footer from '../Componentes/Footer/footer.jsx';
 
 function Home() {
   const [showLogo, setShowLogo] = useState(false);
@@ -23,34 +15,14 @@ function Home() {
 
   useEffect(() => {
     const handleScroll = () => {
-      // Obtener las secciones de contacto y footer
-      const contactoSection = document.getElementById('contacto');
-      const footerSection = document.getElementById('footer');
       const separadorSection = document.getElementById('separador');
 
       if (separadorSection) {
         const separadorRect = separadorSection.getBoundingClientRect();
-        const contactoRect = contactoSection
-          ? contactoSection.getBoundingClientRect()
-          : null;
-        const footerRect = footerSection
-          ? footerSection.getBoundingClientRect()
-          : null;
 
-        // Verificar si estamos en la sección de contacto o footer
-        const inContacto = contactoRect
-          ? contactoRect.top <= window.innerHeight && contactoRect.bottom >= 0
-          : false;
-        const inFooter = footerRect
-          ? footerRect.top <= window.innerHeight && footerRect.bottom >= 0
-          : false;
-
-        // Mostrar el logo solo si estamos en la sección separador y no en contacto o footer
+        // Mostrar el logo solo si estamos en la sección separador
         const isVisible =
-          separadorRect.top <= window.innerHeight &&
-          separadorRect.bottom >= 0 &&
-          !inContacto &&
-          !inFooter;
+          separadorRect.top <= window.innerHeight && separadorRect.bottom >= 0;
 
         setShowLogo(isVisible);
       }
@@ -74,24 +46,18 @@ function Home() {
       {showBanner && <BannnerSuscribe onClose={handleClose} />}
       <Headers />
 
-      {/* Agregamos el NavBar aquí, si es parte del header */}
       {/* Contenido de la primera sección */}
       <div id="separador">
         <ResponsiveImageTextComponent id="responsiveImageTextComponent" />
 
         <BannerINversion />
         <div className="bg-[#F2F2F2]">
-          {' '}
           <Faqs></Faqs>
           <br></br>
           <Contacto id="contacto" />
-        </div>
-
-        <div id="footer">
-          <Footer />
+          <Footer></Footer>
         </div>
       </div>
-      {/* Resto del contenido */}
 
       {/* Logo flotante, visible solo si showLogo es true */}
       {showLogo && (
